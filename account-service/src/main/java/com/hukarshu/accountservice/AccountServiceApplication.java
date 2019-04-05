b9443a15429aeafdefef659e68d245cd81575e7e
hukarshu <1974921044@qq.com>
2019-04-05 17:20:29 +0800

    account-service

---
+++ account-service/src/main/java/com/hukarshu/accountservice/AccountServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hukarshu.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+@EnableOAuth2Client
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
